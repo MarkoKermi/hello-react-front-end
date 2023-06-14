b4a0559a6997f6d64a144e30d19a69ea7885754f
Marko Kermiciev <kermicievmarko@gmail.com>
2023-06-14 11:52:07 +0200

    setup linters and fix the linters errors

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,6 @@
-// import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-// import reducer from './reducers';
-
-// const store = createStore(reducer, applyMiddleware(thunk));
-
-// export default store;
 import { configureStore } from '@reduxjs/toolkit';
-import reducer from './reducers';
 import thunk from 'redux-thunk';
-
+import reducer from './reducers';
 
 const store = configureStore({
   reducer,
